a6fa0c551be9f6af4795fd8b778f5a51ad4fa6c2
Greg Bahr <gregsbahr@gmail.com>
2019-12-01 00:07:15 +0000

    plurallized table names to get around using SQL keywords

--- schema/ddl.sql
+++ schema/ddl.sql
@@ -1,49 +1,49 @@
 CREATE DATABASE blogs;
 USE Blogs;
 
-CREATE TABLE User (
+CREATE TABLE Users (
     username VARCHAR(100) PRIMARY KEY,
     name VARCHAR(100),
     password VARCHAR(100),
     email VARCHAR(100)
-)
+);
 
-CREATE TABLE Blog (
+CREATE TABLE Blogs (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
     description VARCHAR(100),
     owner VARCHAR(100),
-    FOREIGN KEY (owner) REFERENCES User (username)
+    FOREIGN KEY (owner) REFERENCES Users (username)
 );
 
-CREATE TABLE Post (
+CREATE TABLE Posts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     blogId INT,
     title VARCHAR(1000),
     author VARCHAR(100),
     content TEXT,
-    FOREIGN KEY (author) REFERENCES User (username),
-    FOREIGN KEY (blogId) REFERENCES Blog (id)
+    FOREIGN KEY (author) REFERENCES Users (username),
+    FOREIGN KEY (blogId) REFERENCES Blogs (id)
 );
 
-CREATE TABLE Comment (
+CREATE TABLE Comments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     postId INT,
     author VARCHAR(100),
     content TEXT,
-    FOREIGN KEY (author) REFERENCES User (username),
-    FOREIGN KEY (postId) REFERENCES Post (id)
+    FOREIGN KEY (author) REFERENCES Users (username),
+    FOREIGN KEY (postId) REFERENCES Posts (id)
 );
 
-CREATE TABLE Tag (
+CREATE TABLE Tags (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30)
 );
 
-CREATE TABLE PostTag (
+CREATE TABLE PostTags (
     postId INT,
     tagId INT,
     PRIMARY KEY (postId, tagId),
-    FOREIGN KEY (tagId) REFERENCES Tag (id),
-    FOREIGN KEY (postId) REFERENCES Post (id)
+    FOREIGN KEY (tagId) REFERENCES Tags (id),
+    FOREIGN KEY (postId) REFERENCES Posts (id)
 );
