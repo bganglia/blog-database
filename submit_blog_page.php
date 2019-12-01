23c47e9508883315509601e64436e5ed118094e9
bganglia <48276939+bganglia@users.noreply.github.com>
2019-12-01 10:49:30 -0600

    Basic post insertion now works

--- submit_blog_page.php
+++ submit_blog_page.php
@@ -1,39 +1,37 @@
 <!DOCTYPE HTML>
 <head> </head>
 <body>
-<!-- Code projects against SQL injection, perhaps unnecessarily. -->
-<!-- FYI you must add the host and password yourself. They come from the code examples given out in class. -->
-<!-- Currently, this code simply creates a post without actually linking it to any blog. This will have to be fixed. -->
 <?php
     //Connect to database
     $dbhost = "localhost";
     $dbusername = "root";
-    $dbpassword = ""; // not sure if I should be committing thiS
-    $dbname = "blogs";
+    $dbpassword = "";
+    $dbname = "Blogs";
     $conn = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+    if ($conn) {
+      echo "working";
+    }
     //Create query
-    $insertion = $conn->prepare("INSERT INTO Post(pid, postTitle, subTitle, shortTitle, summary, text_content)".
-                                "VALUES(?, ?, ?, ?, ?, ?)")
-    //Extract and bind values
-    function columnFromTable($table, $column) {
-      // ... still has to be written ...
+    $text='INSERT INTO Posts(blogId, title, content, author) VALUES (?, ?, ?, ?)';
+    echo $text;
+    $insertion = $conn->prepare($text);
+    if ($insertion) {
+      echo "working";
     }
-    function getBlogPID() {
-     // ... still has to be written ...
-     // current plan is to take the maximum pid + 1.
-     // this is not scaleable but will get the page on its feet.
-    }
-    $pid = getBlogPID();
-    $postTitle = $_POST("title");
-    $subTitle = $_POST("subtitle");
-    $shortTitle = $_POST("short_title");
-    $summary = $_POST("summary");
-    $text_content = $_POST("content");
-    $insertion->bind_param("ssssss", $pid, $postTitle, $subTitle, $shortTitle, $summary, $text_content);
+    $blogId = (int) $_POST["blogId"];
+    $title = $_POST["title"];
+    $content = $_POST["content"];
+    $author = $_POST["author"];
+    echo $blogId;
+    echo $title;
+    echo $content;
+    echo $author;
+    //echo $insertion;
+    $insertion->bind_param("isss", $blogId, $title, $content, $author);
+    echo $insertion->execute();
+    $insertion->close();
     //Wrap up
-    $conn->close()
-    echo "Your post has been recieved"
+    $conn->close();
+    echo "Your post has been recieved";
 ?>
-<!-- It might be nice to redirect to the actual post. -->
-</body>
-
+<body>
