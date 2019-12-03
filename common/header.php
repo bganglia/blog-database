e586222cfc96a550908c56220096f4abd36d6d6e
Greg Bahr <gregsbahr@gmail.com>
2019-12-03 02:42:34 +0000

    title links to home

--- common/header.php
+++ common/header.php
@@ -1,8 +1,7 @@
 <div class="mb-5 border-bottom">
   <div class="d-flex flex-row justify-content-between align-items-center mt-2 mb-2 mr-auto ml-auto" style="width: 60%;">
-    <h1>Database Blog</h1>
+    <h1><a href="/home">Database Blog</a></h1>
     <div class="d-flex flex-row justify-content-between">
-        Links here
     </div>
   </div>
 </div>
