1af2afcd692baf7da2eaa23b8606a3678ff84319
PM <morrisonp@fontbonne.edu>
2019-12-04 16:29:12 -0500

    add search bar to header

--- common/header.php
+++ common/header.php
@@ -2,6 +2,10 @@
   <div class="d-flex flex-row justify-content-between align-items-center mt-2 mb-2 mr-auto ml-auto" style="width: 60%;">
     <h1><a href="/home">Database Blog</a></h1>
     <div class="d-flex flex-row justify-content-between align-items-center">
+      <form action=search_results.php>
+        <input type="text" name="search">
+        <input type="submit" name="submit" value="Search">
+      </form>
       <?php
         if (!isset($_SESSION)) {
           session_start();
